00bbb304af815e6a2c182901702287fedf4ce4b9
whoareyou <krbavadiya11@gmail.com>
2025-06-29 10:42:24 +0530

    in reduxtoolkit todo app, updateTodo is still remaining... (pending)

--- reduxtoolkitTodo/src/app/store.js
+++ reduxtoolkitTodo/src/app/store.js
@@ -6,3 +6,8 @@ import todoReducer from '../features/todo/todoSlice';
 export const store = configureStore({
     reducer: todoReducer // here we can give the whole list of reducers, but since we have only one slice, we will just give the todoReducer.
 })
+
+
+// All applications have only one store.. usse zyada lena is strictly not advised at all.. and also the store is the one and only source of truth for the application..
+
+// Ye ek syntax hai documentation me.. that we call features as slice ..
